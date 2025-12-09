bfc3161a25d8120fd41436114d861c5f48e5e48a
sunnyluo1985 <luoxuansmile1985@gmail.com>
2025-12-09 16:57:00 +0800

    Fix wrangler.json syntax

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,13 @@
+<content><![CDATA[/// <reference types="vite/client" />
 declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly API_KEY: string;
-    readonly VITE_API_KEY: string;
-    [key: string]: string | undefined;
-  }
+interface ProcessEnv {
+readonly API_KEY: string;
+readonly VITE_API_KEY: string;
+[key: string]: string | undefined;
 }
+}
+declare var process: {
+env: NodeJS.ProcessEnv;
+};
+]]></content>
+</changes>
