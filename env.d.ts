ae668243a4cb3bd60cdc82f0e3fd6ff43e48c28f
sunnyluo1985 <luoxuansmile1985@gmail.com>
2025-12-09 16:03:30 +0800

    Fix TypeScript env types for Cloudflare build

--- env.d.ts
+++ env.d.ts
@@ -1,15 +1,7 @@
-Updated files:
-env.d.ts
-<description>修正文件内容为真正的 TypeScript 类型声明，替换掉错误的中文描述。</description>
-<content><![CDATA[/// <reference types="vite/client" />
 declare namespace NodeJS {
-interface ProcessEnv {
-readonly API_KEY: string;
-readonly VITE_API_KEY: string;
-[key: string]: string | undefined;
+  interface ProcessEnv {
+    readonly API_KEY: string;
+    readonly VITE_API_KEY: string;
+    [key: string]: string | undefined;
+  }
 }
-}
-declare var process: {
-env: NodeJS.ProcessEnv;
-};
-]]></content>
