cb42f7e77ef0f5c113ab54ae32339318cd10c621
sunnyluo1985 <luoxuansmile1985@gmail.com>
2025-12-09 17:05:16 +0800

    Fix wrangler.json content - remove invalid tags

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,4 @@
-<content><![CDATA[/// <reference types="vite/client" />
-declare namespace NodeJS {
+{
 interface ProcessEnv {
 readonly API_KEY: string;
 readonly VITE_API_KEY: string;
@@ -8,6 +7,4 @@ readonly VITE_API_KEY: string;
 }
 declare var process: {
 env: NodeJS.ProcessEnv;
-};
-]]></content>
-</changes>
+}
